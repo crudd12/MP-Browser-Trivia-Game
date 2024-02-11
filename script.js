2cdadacf808acf4a2d0f84386f6cb97885dcf8de
crudd12 <crudd12@gmail.com>
2024-02-11 14:25:04 -0800

    updated code for pulling trivia options

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const triviaData = [
     {
         "question": "In which city did Kareem Abdul-Jabbar break Wilt Chamberlain's record for most career points in NBA History?",
-        "rightAnswer": "Las Vegas",
-        "wrongAnswer1": "New York City",
-        "wrongAnswer2": "Los Angeles",
-        "wrongAnswer3": "Chicago",
+        // "rightAnswer": "Las Vegas",
+        "wrongAnswers":
+        {
+            a: "New York City",
+            rightAnswer: 'Las Vegas',
+            b: "Los Angeles",
+            c: "Chicago"
+        }
     },
     {
         "question": "Laker head coach Phil Jackson retired as a player with the New Jersey Nets in 1980. Which other team did he play for in his career?",
-        "rightAnswer": "New York Knicks",
-        "wrongAnswer1": "San Antonio Spurs",
-        "wrongAnswer2": "Los Angeles Clippers",
-        "wrongAnswer3": "Charlotte Hornets",
+        // "rightAnswer": "New York Knicks",
+        "wrongAnswers":
+        {
+            a: "San Antonio Spurs",
+            b: "Los Angeles Clippers",
+            c: "Charlotte Hornets",
+            rightAnswer: 'New York Knicks'
+        }
     }
 ];
 
@@ -22,8 +30,28 @@ function getTriviaData() {
     console.log(triviaQuestion, triviaAnswer);
 
     function displayTriviaData() {
-        document.querySelector('#trivia-section').textContent = triviaQuestion;
+        // document.querySelector('#trivia-section').textContent = triviaQuestion;
+        document.querySelector('#trivia-question').textContent = triviaQuestion;
+        // const triviaOptions = `${triviaData[0].rightAnswer}, ${triviaData[0].wrongAnswer1}, ${triviaData[0].wrongAnswer2}, ${triviaData[0].wrongAnswer3}`
+        let triviaOptions = triviaData[0].wrongAnswers;
+        console.log(triviaOptions);
+        const ul = document.createElement('ul');
+        triviaOptions.appendChild(ul);
+        li.textContent = `${triviaData[0].rightAnswer}`;
+        // document.querySelector('#trivia-answers').textContent = 
 
+
+
+        //     const triviaAnswers = document.querySelector('#trivia-answers');
+        //    const ul = document.createElement('ul');
+        //     triviaAnswers.appendChild(ul);
+        // li.textContent = `${triviaData[0].rightAnswer}`;
+        // for (let i=0; i < triviaData; i++ {
+        //     const answersList = triviaData[i];
+        //     const li = document.createElement('li');
+        //     li.textContent = 
+        // })
+        // ul.appendChild(li);
         // addTriviaQuestion.textContent(triviaQuestion);
         // addTriviaQuestion.append(test);
         // document.getElementById("trivia-section").textContent = triviaQuestion;
